b63f175678d2c82f3aee7425b70217e4d9fd8aac
ManuelCastejon <manuel.castejon@outlook.com>
2019-02-15 16:52:30 +0100

    Añadido detalle de categoría
    Añadido detalle de categoría

--- controladores/CategoriaController.php
+++ controladores/CategoriaController.php
@@ -19,6 +19,35 @@ class CategoriaController
         $this->view->show("categorias/listado.php", $data);        
     }
 
+    public function detalle(){
+         
+        if(isset($_GET['id']))
+        {    
+            $id = $_GET['id'];    
+            //Guardamos en un array los datos a mostrar en la vista
+            $index['titulo'] = "Categoria";
+            $index['texto'] = "Estas en la página de Detalle de Categoria id $id";
+            //Pasamos a la vista toda la información que se desea representar
+            $data['datos'] = $index;
+            //Finalmente presentamos nuestra plantilla
+        }
+        else
+        {
+            //Guardamos en un array los datos a mostrar en la vista
+            $index['titulo'] = "Categoria";
+            $index['texto'] = "Error.. Categoría no encontrada";
+            //Pasamos a la vista toda la información que se desea representar
+            $data['datos'] = $index;  
+
+        }         
+        $this->view->show("categorias/detalle.php", $data);        
+    }
+
+
+
+
+
+
     public function crear(){
         //Guardamos en un array los datos a mostrar en la vista
         $index['titulo'] = "Categoria";

0058d776dd93f8dbe01891339b526a6e73286f98
ManuelCastejon <manuel.castejon@outlook.com>
2019-02-21 19:49:14 +0100

    Controlador Categoria+Articulo OK
    Completado Controladores Categorias y Artículos

--- controladores/ArticuloController.php
+++ controladores/ArticuloController.php
@@ -8,7 +8,8 @@ class ArticuloController
         $this->view = new View();
     }  
      
-    
+    // lista Todos los artículos
+    // index.php?controlador=articulo&accion=listado 
     public function listado(){
         //Guardamos en un array los datos a mostrar en la vista
         $index['titulo'] = "Articulo/Listado";
@@ -18,9 +19,10 @@ class ArticuloController
         //Finalmente presentamos nuestra plantilla         
         $this->view->show("articulos/listado.php", $data);        
     }
-
+    
+    // accede al detalle del Artículo
     public function detalle(){
-         
+    // index.php?controlador=articulo&accion=detalle&id=33     
         if(isset($_GET['id']))
         {    
             $id = $_GET['id'];    
@@ -47,8 +49,9 @@ class ArticuloController
 
 
 
-    // crear ( FORUMULARIO ) 
+    // crear ( muestra un FORUMULARIO ) 
     public function crear(){
+    // index.php?controlador=articulo&accion=crear
         //Guardamos en un array los datos a mostrar en la vista
         $index['titulo'] = "Artículo/Crear";
         $index['texto'] = "Estas en la página de Crear una Artículo";
@@ -62,10 +65,8 @@ class ArticuloController
 
 
     // insertar ( obtiene los datos del Form POST )
-    public function insertar(){
-    
- 
-
+    public function insertar(){ 
+        // index.php?controlador=articulo&accion=insertar desde el FORM
         if(isset($_POST['Nombre']) & isset($_POST['Descripcion']))
         {        
             // se envía con el formulario
@@ -81,11 +82,22 @@ class ArticuloController
         }       
     }
 
-    // editar ( FORMULARIO )
-    // actualizar ( obtiene los datos del Form PUT/POST )
-    
-    
     // eliminar
+    public function eliminar(){
+        // index.php?controlador=articulo&accion=eliminar&id=33
+        if(isset($_REQUEST['id']))
+        {       
+            $id = $_REQUEST['id'];
+            //Guardamos en un array los datos a mostrar en la vista
+            $index['titulo'] = "Eliminar Artículo";
+            $index['texto'] = "Estas en la página de Eliminar Artículo con ID igual a $id";
+            //Pasamos a la vista toda la información que se desea representar
+            $data['datos'] = $index;
+            //Finalmente presentamos nuestra plantilla         
+            $this->view->show("articulos/eliminar.php", $data);   
+        
+        }
+    }
  
 
  

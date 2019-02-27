4639311cbee45f3c6b3afb80438a0161470472a6
ManuelCastejon <manuel.castejon@outlook.com>
2019-02-27 17:54:59 +0100

    Explicacion listadoTotal Usuarios

--- vistas/index.php
+++ vistas/index.php
@@ -9,13 +9,16 @@
     </tr>  
 
      
-<?php
+<?php // Extraer TODAS las FILAS de la tabla USUARIO
     while($usuario = $listado_usuarios->fetch())
     {
     ?>
-    <tr>
+    <tr>  <?php // Imprime el valor de todas las COLUMNAS ?>
         <td><?php echo $usuario['idUsuario']?></td>
         <td><?php echo $usuario['Nombre']?></td>
+        <td><?php echo $usuario['Password']?></td>
+        <td><?php echo $usuario['Email']?></td>
+        <td><?php echo $usuario['Estado']?></td>
     </tr>
     <?php
     }
